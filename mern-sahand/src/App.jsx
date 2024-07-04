74a56d59c9005a613b8df0f2de6286dba76bcce7
Umiar-20 <swaso.awijaya@gmail.com>
2024-07-04 14:34:48 +0700

    create header section

--- mern-sahand/src/App.jsx
+++ mern-sahand/src/App.jsx
@@ -5,10 +5,12 @@ import Projects from "./pages/Projects";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />

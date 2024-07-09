51f0c4f49eb96291e32d5247c9c2c37e336e6f6e
Umiar-20 <swaso.awijaya@gmail.com>
2024-07-09 16:27:02 +0700

    building cards for notes

--- Notes-APP_time to program/frontend/notes-app/src/pages/Home/Home.jsx
+++ Notes-APP_time to program/frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,26 @@
+import NoteCard from "../../components/Cards/NoteCard";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Home = () => {
   return (
-    <>
+    <div>
       <Navbar />
-    </>
+
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting"
+            date="3rd April 2024"
+            content="Meeting April, 7th 2024"
+            tags="#meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 export default Home;
